1c4ae2c5077dd6531ad988c1af2e3e96ed64a323
Carlos Beltran <cbeltr4@gmail.com>
2014-10-24 19:22:08 -0500

    First commit

--- Age of Laika/ViewController.swift
+++ Age of Laika/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ageField: UITextField!
+    @IBOutlet weak var convertedAgeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,54 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButton(sender: AnyObject) {
+        let conversion = 7
+        let fieldToInt = ageField.text.toInt()!
+        var newDouble = Double(fieldToInt)
+        
+        
+        if fieldToInt < 0 {
+            convertedAgeLabel.text = "Cannot have non-positive value!"
+            convertedAgeLabel.hidden = false
+            ageField.text = ""
+            ageField.resignFirstResponder()
+            return
+        }
+        
+        convertedAgeLabel.text = "In dog years: \(fieldToInt * conversion) "
+        convertedAgeLabel.hidden = false
+        
+        ageField.text = ""
+        ageField.resignFirstResponder()
+    }
 
+    @IBAction func convertRealButton(sender: AnyObject) {
+        let firstConversion = 10.5
+        let secondConversion:Double = 4
+        
+        var ageEntered = ageField.text.toInt()!
+        var age:Double
+        
+        if ageEntered < 0 {
+            convertedAgeLabel.text = "Cannot have non-positive value!"
+            convertedAgeLabel.hidden = false
+            ageField.text = ""
+            ageField.resignFirstResponder()
+            return
+        }
+        
+        if ageEntered <= 2 {
+            age = Double(ageEntered) * firstConversion
+        }
+        else {
+            age = ((Double(ageEntered) - 2.0) * secondConversion) + (2.0 * firstConversion)
+        }
+        
+        convertedAgeLabel.text = "In real dog years: \(age) "
+        convertedAgeLabel.hidden = false
+        
+        ageField.text = ""
+        ageField.resignFirstResponder()
+    }
 }
 
